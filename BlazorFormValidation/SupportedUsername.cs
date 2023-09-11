76cb7858424fa19ce31b7baa7f4bd4b7caa61490
Claudio Bernasconi <email@claudio-bernasconi.net>
2023-09-12 00:56:00 +0200

    Add custom validation attribute

---
+++ BlazorFormValidation/SupportedUsername.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormValidation;
+
+public class SupportedUsername : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value,
+        ValidationContext validationContext)
+    {
+        var username = (string)value;
+        if (username == "Bill Gates")
+        {
+            return new ValidationResult("You cannot use the reserved username 'Bill Gates'.",
+                new[] { validationContext.MemberName });
+        }
+
+        return ValidationResult.Success!;
+    }
+}
